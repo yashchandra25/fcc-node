6aa8cad6790a3084c9fa78bd0f777a92deb56bf0
yashchandra25 <114137761+yashchandra25@users.noreply.github.com>
2023-01-18 18:32:53 +0530

    till modules

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 console.log("Hello, World!");
+
+// ? prints name of current directory
+console.log(__dirname);
+
+// ? prints name of current file
+console.log(__filename);
+
+/* // ! will print every 1 sec infinite loop
+setInterval(() => {
+  console.log("This will print after 1 sec");
+}, 1000);
+*/
+
+// * Modules
+// ? Modules - Encapsulated Code (Only share minimum)
+// ! Node is built upon commonJS, every file is module
+// ! CommonJS is a project with the goal to establish conventions on the module ecosystem for JavaScript outside of the web browser.
+const names = require("./test-module-1");
+console.log(names);
+const sayHi = (name) => {
+  console.log(`Hello there ${name}`);
+};
+sayHi("Yash");
+sayHi(names[0]);
+sayHi(names[1]);
---
+++ test-module-1.js
@@ -0,0 +1,5 @@
+const secret = "I am not telling you my password";
+const randomPerson1 = "abc";
+const randomPerson2 = "xyz";
+console.log(module);
+module.exports = { randomPerson1, randomPerson2 };
